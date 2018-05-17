1d95d236b2e99a19340167f53462874f0f3b5bc0
emaronti <emaronti@hotmail.it>
2018-05-17 08:11:32 -0400

    commit 24

---
+++ Esercizio_cinema/index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    
+    <link rel="stylesheet" href="cinema.css" type="text/css" />
+  </head>
+  
+  <body>
+    
+    <div align="center" class="listaF">
+      <h1 id="listf">Lista film</h1>
+      
+    </div>
+    
+    
+    
+    
+  </body>
+</html>
